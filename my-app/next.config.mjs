250f7337140fe356fa63eddcc7fc1ca7e672b236
Aqil Fasih <aqilfasihsb11@gmail.com>
2024-10-02 11:37:41 +0500

    today

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "a2.espncdn.com",
+        },
+        {
+          protocol: "https",
+          hostname: "compote.slate.com",
+        },
+        {
+          protocol: "https",
+          hostname: "www.usmagazine.com",
+        },
+        
+      ],
+    },
+  };
+
 
 export default nextConfig;
--- my-app/src/app/blogs/page.js
+++ my-app/src/app/blogs/page.js
@@ -1,7 +1,45 @@
-export default function Blogs(){
-    return (
-        <div className="text-center justify-center mt-28 bg-purple-300" > 
-        <h1 className="font-bold text-5xl text-black " > Hello Blogs</h1>
-        </div>
-    );
-}
+// import link from "next/link";
+// import React from "react"
+
+// const Blogs = async () => {
+//     let news = await fetch(
+//         "https://api.thenewsapi.com/v1/news/top?api_token=026l85WrTZtvYR9AR2WxXUNI6iJmiTFZNq0oZI4B&locale=us&limit=3"
+//     );
+//     news = await news.json();
+//     return (
+//       //  <div className="">
+//         // {news?.formData.map((data)=> }
+//        //  </div>
+        
+//         <div className="text-center justify-center mt-28 bg-purple-300" > 
+//         <h1 className="font-bold text-5xl text-black " > Hello Blogs</h1>
+//         </div>
+//     );
+//     }
+
+// export default Blogs;
+
+import Link from "next/link";
+import React from "react";
+
+const Blogs = async () => {
+  let news = await fetch(
+    "https://api.thenewsapi.com/v1/news/top?api_token=4kg9t6R9h2j1gFnP6oerJz0h6BHr9ZebTXA3GUl6&locale=us&limit=3"
+  );
+  news = await news.json();
+  return (
+    <div className="">
+      {news?.data.map((data) => (
+        <Link href={`/blogs/${data.uuid}`} key={data.uuid} className="border m-2 rounded p-4 flex">
+          <img src={data.image_url} className="h-20 w-20 rounded" />
+          <div className="ml-2">
+            <h1 className="font-bold">{data?.title}</h1>
+            <h1 className="my-2">{data?.description}</h1>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
